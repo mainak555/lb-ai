67bdd7c641e0b3b097e7ee31a230734d24953882
mainak555 <mainak555@github.com>
2019-05-25 01:28:31 +0530

    Fixed lambdas

--- Squash.py
+++ Squash.py
@@ -4,118 +4,223 @@ class Squash:
     def __init__(self):
         self.function = {
             'sigmoid': lambda x: 1 / (1 + np.exp(-x)),
-            'swish': lambda x: x / (1 + np.exp(-x)),
             'tanh': lambda x: np.tanh(x),
-            'softmax': lambda x, exps = np.exp(x): exps / float(sum(exps)),
             'arctan': lambda x: np.arctan(x),
+            'softmax': lambda x: (lambda exps= np.exp(x): exps / float(sum(exps)))(),
+            'softplus': lambda x: np.log(1 + np.exp(x)),
+            'swish': lambda x: x / (1 + np.exp(-x)),
             'identity': lambda x: x,
-
+            'relu': lambda x: x if x >= 0 else np.zeros(x.shape[0]),
+            'prelu': lambda x, alpha: x if x >= 0 else alpha * x,
+            'elu': lambda x, alpha: [item if item >= 0 else np.dot(alpha, np.exp(item) - 1) for item in x]
             }            
         
         self.derivative = {
-            'sigmoid': lambda x, fx = self.function['sigmoid'](x): fx * (1 - fx),
-            'swish': lambda x, fx = x * self.function['sigmoid'](x): fx + (1 - fx) * self.function['sigmoid'](x),
-            'tanh': lambda x, fx = self.function['tanh'](x): 1 - fx**2,
-            'softmax': lambda x, fx = self.function['softmax'](x): fx * (1 - fx),
+            'sigmoid': lambda x: (lambda fx= self.function['sigmoid'](x): fx * (1 - fx))(),
+            'tanh': lambda x: (lambda fx = self.function['tanh'](x): 1 - fx**2)(),
             'arctan': lambda x: 1 / (1 + x**2),
-            'identity': lambda x: 1,
+            'softmax': lambda x: (lambda fx= self.function['softmax'](x): fx * (1 - fx))(),
+            'softplus': lambda x: self.function['sigmoid'](x),
+            'swish': lambda x: (lambda x= x, fx= x * self.function['sigmoid'](x): fx + (1 - fx) * self.function['sigmoid'](x))(),
+            'identity': lambda x: np.ones(x.shape[0]),
+            'relu': lambda x: np.ones(x.shape[0]) if x >= 0 else np.zeros(x.shape[0]),
+            'prelu': lambda x, alpha: np.ones(x.shape[0]) if x >= 0 else np.repeat(alpha, x.shape[0]),
+            'elu': lambda x, alpha: [1 if item >= 0 else sum(self.function['elu']([item], alpha), alpha) for item in x]
         }
 #%%
 import matplotlib.pyplot as plt
 activation = Squash()
-z = np.arange(-10, 10, 0.001)
+z = np.arange(-5, 5, 0.1)
+prn = lambda i, fx, d_fx: print('>{}\n-f(x): {}\n-df/dx: {}'.format(i, fx, d_fx))
 #%%
 #Sigmoid
-fig, ax = plt.subplots(4, 3, sharex='all', figsize=(10, 20))
-x = z
+fig, ax = plt.subplots(4, 3, figsize=(10, 20))
+a = z
 for i in range(4):
-    fx = activation.function['sigmoid'](x)
+    fx = activation.function['sigmoid'](a)
     ax[i, 0].set_title('f(x)')
     ax[i, 0].set_xlabel('x->')
-    ax[i, 0].plot(x, fx)    
-    ddx_of_fx = activation.derivative['sigmoid'](x)
-    ax[i, 1].set_title('d/dx f(x)')
+    ax[i, 0].plot(a, fx)    
+    ddx_of_fx = activation.derivative['sigmoid'](a)
+    ax[i, 1].set_title('df/dx')
     ax[i, 1].set_xlabel('f(x)->')
     ax[i, 1].plot(fx, ddx_of_fx)
-    ax[i, 2].set_title('d/dx f(x)')
-    ax[i, 2].set_xlabel('input->')
+    ax[i, 2].set_title('df/dx vs. inp')
+    ax[i, 2].set_xlabel('inp->')
     ax[i, 2].plot(z, ddx_of_fx)
-    print('>{}\n-f(x): {}\n-d/dx f(x): {}'.format(i, fx, ddx_of_fx))
-    x = fx
+    prn(i, fx, ddx_of_fx)
+    a = fx
 plt.show()
 #%%
 #Tanh
 fig, ax = plt.subplots(4, 3, sharex='all', figsize=(10, 20))
-x = z
+a = z
 for i in range(4):
-    fx = activation.function['tanh'](x)
+    fx = activation.function['tanh'](a)
     ax[i, 0].set_title('f(x)')
     ax[i, 0].set_xlabel('x->')
-    ax[i, 0].plot(x, fx)    
-    ddx_of_fx = activation.derivative['tanh'](x)
-    ax[i, 1].set_title('d/dx f(x)')
+    ax[i, 0].plot(a, fx)    
+    ddx_of_fx = activation.derivative['tanh'](a)
+    ax[i, 1].set_title('df/dx')
     ax[i, 1].set_xlabel('f(x)->')
     ax[i, 1].plot(fx, ddx_of_fx)
-    ax[i, 2].set_title('d/dx f(x)')
-    ax[i, 2].set_xlabel('input->')
+    ax[i, 2].set_title('df/dx vs. inp')
+    ax[i, 2].set_xlabel('inp->')
     ax[i, 2].plot(z, ddx_of_fx)
-    print('>{}\n-f(x): {}\n-d/dx f(x): {}'.format(i, fx, ddx_of_fx))
-    x = fx
+    prn(i, fx, ddx_of_fx)
+    a = fx
 plt.show()
 #%%
 #ArcTan
 fig, ax = plt.subplots(4, 3, sharex='all', figsize=(10, 20))
-x = z
+a = z
 for i in range(4):
-    fx = activation.function['arctan'](x)
+    fx = activation.function['arctan'](a)
     ax[i, 0].set_title('f(x)')
     ax[i, 0].set_xlabel('x->')
-    ax[i, 0].plot(x, fx)    
-    ddx_of_fx = activation.derivative['arctan'](x)
-    ax[i, 1].set_title('d/dx f(x)')
+    ax[i, 0].plot(a, fx)    
+    ddx_of_fx = activation.derivative['arctan'](a)
+    ax[i, 1].set_title('df/dx')
     ax[i, 1].set_xlabel('f(x)->')
     ax[i, 1].plot(fx, ddx_of_fx)
-    ax[i, 2].set_title('d/dx f(x)')
-    ax[i, 2].set_xlabel('input->')
+    ax[i, 2].set_title('df/dx vs. inp')
+    ax[i, 2].set_xlabel('inp->')
     ax[i, 2].plot(z, ddx_of_fx)
-    print('>{}\n-f(x): {}\n-d/dx f(x): {}'.format(i, fx, ddx_of_fx))
-    x = fx
+    prn(i, fx, ddx_of_fx)
+    a = fx
 plt.show()
 #%%
 #Softmax
 fig, ax = plt.subplots(4, 3, sharex='all', figsize=(10, 20))
-x = z
+a = z
 for i in range(4):
-    fx = activation.function['softmax'](x)
+    fx = activation.function['softmax'](a)
     ax[i, 0].set_title('f(x)')
     ax[i, 0].set_xlabel('x->')
-    ax[i, 0].plot(x, fx)    
-    ddx_of_fx = activation.derivative['softmax'](x)
-    ax[i, 1].set_title('d/dx f(x)')
+    ax[i, 0].plot(a, fx)    
+    ddx_of_fx = activation.derivative['softmax'](a)
+    ax[i, 1].set_title('df/dx')
     ax[i, 1].set_xlabel('f(x)->')
     ax[i, 1].plot(fx, ddx_of_fx)
-    ax[i, 2].set_title('d/dx f(x)')
-    ax[i, 2].set_xlabel('input->')
+    ax[i, 2].set_title('df/dx vs. inp')
+    ax[i, 2].set_xlabel('inp->')
     ax[i, 2].plot(z, ddx_of_fx)
-    print('>{}\n-f(x): {}\n-d/dx f(x): {}'.format(i, fx, ddx_of_fx))
-    x = fx
+    prn(i, fx, ddx_of_fx)
+    a = fx
+plt.show()
+#%%
+#SoftPlus
+fig, ax = plt.subplots(4, 3, sharex='all', figsize=(10, 20))
+a = z
+for i in range(4):
+    fx = activation.function['softplus'](a)
+    ax[i, 0].set_title('f(x)')
+    ax[i, 0].set_xlabel('x->')
+    ax[i, 0].plot(a, fx)    
+    ddx_of_fx = activation.derivative['softplus'](a)
+    ax[i, 1].set_title('df/dx')
+    ax[i, 1].set_xlabel('f(x)->')
+    ax[i, 1].plot(fx, ddx_of_fx)
+    ax[i, 2].set_title('df/dx vs. inp')
+    ax[i, 2].set_xlabel('inp->')
+    ax[i, 2].plot(z, ddx_of_fx) 
+    prn(i, fx, ddx_of_fx)   
+    a = fx
 plt.show()
 #%%
 #Swish
 fig, ax = plt.subplots(4, 3, sharex='all', figsize=(10, 20))
-x = z
+a = z
 for i in range(4):
-    fx = activation.function['swish'](x)
+    fx = activation.function['swish'](a)
     ax[i, 0].set_title('f(x)')
     ax[i, 0].set_xlabel('x->')
-    ax[i, 0].plot(x, fx)    
-    ddx_of_fx = activation.derivative['swish'](x)
-    ax[i, 1].set_title('d/dx f(x)')
+    ax[i, 0].plot(a, fx)    
+    ddx_of_fx = activation.derivative['swish'](a)
+    ax[i, 1].set_title('df/dx')
     ax[i, 1].set_xlabel('f(x)->')
     ax[i, 1].plot(fx, ddx_of_fx)
-    ax[i, 2].set_title('d/dx f(x)')
-    ax[i, 2].set_xlabel('input->')
-    ax[i, 2].plot(z, ddx_of_fx)
-    print('>{}\n-f(x): {}\n-d/dx f(x): {}'.format(i, fx, ddx_of_fx))
-    x = fx
+    ax[i, 2].set_title('df/dx vs. inp')
+    ax[i, 2].set_xlabel('inp->')
+    ax[i, 2].plot(z, ddx_of_fx) 
+    prn(i, fx, ddx_of_fx)   
+    a = fx
+plt.show()
+#%%
+#Identity
+fig, ax = plt.subplots(2, 3, figsize=(10, 10))
+z = np.arange(-1, 1, .2)
+a = z
+for i in range(2):
+    fx = activation.function['identity'](a)
+    ax[i, 0].set_title('f(x)')
+    ax[i, 0].set_xlabel('x->')
+    ax[i, 0].plot(a, fx)    
+    ddx_of_fx = activation.derivative['identity'](a)
+    ax[i, 1].set_title('df/dx')
+    ax[i, 1].set_xlabel('f(x)->')
+    ax[i, 1].plot(fx, ddx_of_fx)
+    ax[i, 2].set_title('df/dx vs. inp')
+    ax[i, 2].set_xlabel('inp->')
+    ax[i, 2].plot(z, ddx_of_fx) 
+    prn(i, fx, ddx_of_fx)   
+    a = fx
+plt.show()
+#%%
+#ReLu
+fig, ax = plt.subplots(4, 3, sharex='all', figsize=(10, 20))
+a = z
+for i in range(4):
+    fx = activation.function['relu'](a)
+    ax[i, 0].set_title('f(x)')
+    ax[i, 0].set_xlabel('x->')
+    ax[i, 0].plot(a, fx)    
+    ddx_of_fx = activation.derivative['relu'](a)
+    ax[i, 1].set_title('df/dx')
+    ax[i, 1].set_xlabel('f(x)->')
+    ax[i, 1].plot(fx, ddx_of_fx)
+    ax[i, 2].set_title('df/dx vs. inp')
+    ax[i, 2].set_xlabel('inp->')
+    ax[i, 2].plot(z, ddx_of_fx) 
+    prn(i, fx, ddx_of_fx)   
+    a = fx
+plt.show()
+#%%
+#PReLu
+fig, ax = plt.subplots(4, 3, sharex='all', figsize=(10, 20))
+a = z
+for i in range(4):
+    fx = activation.function['prelu'](a)
+    ax[i, 0].set_title('f(x)')
+    ax[i, 0].set_xlabel('x->')
+    ax[i, 0].plot(a, fx)    
+    ddx_of_fx = activation.derivative['prelu'](a)
+    ax[i, 1].set_title('df/dx')
+    ax[i, 1].set_xlabel('f(x)->')
+    ax[i, 1].plot(fx, ddx_of_fx)
+    ax[i, 2].set_title('df/dx vs. inp')
+    ax[i, 2].set_xlabel('inp->')
+    ax[i, 2].plot(z, ddx_of_fx) 
+    prn(i, fx, ddx_of_fx)   
+    a = fx
+plt.show()
+#%%
+#ELu
+fig, ax = plt.subplots(2, 3, sharex='all', figsize=(10, 10))
+a = z
+alpha = 2.25
+for i in range(2):
+    fx = activation.function['elu'](a, alpha)
+    ax[i, 0].set_title('f(x)')
+    ax[i, 0].set_xlabel('x->')
+    ax[i, 0].plot(a, fx)    
+    ddx_of_fx = activation.derivative['elu'](a, alpha)
+    ax[i, 1].set_title('df/dx')
+    ax[i, 1].set_xlabel('f(x)->')
+    ax[i, 1].plot(fx, ddx_of_fx)
+    ax[i, 2].set_title('df/dx vs. inp')
+    ax[i, 2].set_xlabel('inp->')
+    ax[i, 2].plot(z, ddx_of_fx) 
+    prn(i, fx, ddx_of_fx)   
+    a = fx
 plt.show()
