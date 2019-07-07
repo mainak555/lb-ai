7901b89cefab91f32978af3a6dfc00e25c7ff2ed
mainak555 <mainak555@github.com>
2019-07-07 21:11:25 +0530

    updated

--- CreditCardFraudDetection.py
+++ CreditCardFraudDetection.py
@@ -1,6 +1,6 @@
 #%% [markdown]
 ## Credit Card Fraud Detection  
-> Using Keras & AutoEncoders
+#### Using Keras & AutoEncoders
 #%%
 import os
 try:
@@ -16,4 +16,10 @@ import matplotlib.pyplot as plt
 #%%
 df = pd.read_csv('creditcard.csv')
 df[:5]
+#%%
+'''Visualizing Fraud & Non Fraud'''
+from pandas.plotting import scatter_matrix
+scatter_matrix(df[df.columns], figsize=(15,15), alpha=0.2, diagonal='kde')
+plt.show()
+
 #%%
